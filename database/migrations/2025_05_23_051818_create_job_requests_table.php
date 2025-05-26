956394ca1c171789439aee002ee0ebecd2a0f4d7
Schwarzer <johnpaul.vitancol@gmail.com>
2025-05-26 16:23:55 +0800

    [Job Request] - creation of job requests table

---
+++ database/migrations/2025_05_23_051818_create_job_requests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('project_name');
+            $table->string('subject');
+            $table->string('lot_number');
+            $table->integer('status');
+            $table->date('requested_date')->nullable();
+            $table->date('job_ecd');
+            $table->longText('note')->nullable();
+            $table->integer('created_by');
+            $table->integer('updated_by')->default(0);
+            $table->integer('deleted_by')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_requests');
+    }
+};
