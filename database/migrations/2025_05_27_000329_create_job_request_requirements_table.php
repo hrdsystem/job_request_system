46b6819c4b109dfad96d392848cf3338db2f03e9
Schwarzer <johnpaul.vitancol@gmail.com>
2025-05-28 16:37:14 +0800

    [Job Request] - table creation for job requirements

---
+++ database/migrations/2025_05_27_000329_create_job_request_requirements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_request_requirements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('job_request_id');
+            $table->integer('document_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_request_requirements');
+    }
+};
