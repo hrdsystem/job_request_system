32d368969873a695bacdf07185df3841da511670
Schwarzer <johnpaul.vitancol@gmail.com>
2025-06-05 15:35:30 +0800

    [Job Request] - addming of softdeletes for the model

--- app/Models/JobRequest.php
+++ app/Models/JobRequest.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class JobRequest extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $appends = ['checked', 'disabled', 'requirements'];
 
