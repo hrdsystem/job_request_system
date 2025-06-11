5dad56a59a399fecda7ec7c6c5c3234348eeab18
Schwarzer <johnpaul.vitancol@gmail.com>
2025-06-11 16:36:41 +0800

    [Job Request] - computed state data and formattedDate function

--- resources/components/pages/JobRequestPage.vue
+++ resources/components/pages/JobRequestPage.vue
@@ -708,8 +708,29 @@ export default {
             'editMode',
             'filterMode',
             'viewMode',
+            'rules',
             'tableHeight',
-        ])
+            'JobRequestRequiredData',
+            'JobRequestData',
+            'JobRequestRecords',
+            'JobRequestSearch',
+            'JobRequestSort'
+        ]),
+
+        ...mapWritableState(useSampleStore,[
+            'allSelected',
+            'selectedRows'
+        ]),
+
+        formattedDate() {
+            if (!this.tempAddIssuedDate) return '';
+            const date = new Date(this.tempAddIssuedDate);
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+            const day = String(date.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`; // Format: YYYY-MM-DD
+        },
+    },
     }
 }
 </script>
