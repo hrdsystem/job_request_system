f88e615491c6e3ce96af7aa678e0dcc1183195f9
Schwarzer <johnpaul.vitancol@gmail.com>
2025-06-25 16:13:02 +0800

    [Job Request] - eager loading function for JobRequestUpload

--- app/Models/JobRequestRequirement.php
+++ app/Models/JobRequestRequirement.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class JobRequestRequirement extends Model
 {
     use HasFactory;
+
+    public function uploads()
+    {
+        return $this->hasMany(JobRequestUpload::class, 'request_id');
+    }
 }
