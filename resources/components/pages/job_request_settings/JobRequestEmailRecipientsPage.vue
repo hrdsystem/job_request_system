41eeb08394a6ca002bf974f97cdf2d3d053d1e89
Schwarzer <johnpaul.vitancol@gmail.com>
2025-07-15 16:09:34 +0800

    [Job Request] - creation of new page for email recipients

---
+++ resources/components/pages/job_request_settings/JobRequestEmailRecipientsPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+            THIS IS EMAIL RECIPIENT PAGE
+    </v-container>
+</template>
+
+<script setup>
+import ToolBarComponent from '@aspect/ToolBarComponent.vue'
+import SnackBarComponent from '@aspect/SnackBarComponent.vue'
+import FloatButtonComponent from '@aspect/FloatButtonComponent.vue'
+</script>
+
+<script>
+import {mapState,mapActions, mapWritableState} from 'pinia'
+import {useSampleStore} from '../../../js/store'
+export default {
+    data(){
+        return{
+
+        }
+    },
+
+    computed:{
+        ...mapState(useSampleStore,[
+
+        ])
+    },
+
+    method:{
+        ...mapActions(useSampleStore,[
+
+        ])
+    }
+    
+}
+</script>
+
+<style scoped>
+    
+</style>
