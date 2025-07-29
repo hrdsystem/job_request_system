02cf8731a46faaee96abdc971fae9e0e47d7a3af
Schwarzer <johnpaul.vitancol@gmail.com>
2025-07-29 16:10:13 +0800

    [Job Request] - table for email recipients master

--- resources/components/pages/job_request_settings/JobRequestEmailRecipientsPage.vue
+++ resources/components/pages/job_request_settings/JobRequestEmailRecipientsPage.vue
@@ -1,6 +1,36 @@
 <template>
-    <v-container>
-            THIS IS EMAIL RECIPIENT PAGE
+    <v-container class="container-main pt-0">
+    <tool-bar-component :toolbarData="toolbarData"></tool-bar-component>
+        <v-table fixed-header :height="tableHeight" class="mainTable">
+            <thead>
+                <tr>
+                    <th v-show="floatButtonData.editButtonActive" class="text-left" style="width:20px">
+                        <v-checkbox style="display: flex;" color="white" :input-value="allSelected" @change="toggleSelectAll()" ></v-checkbox>
+                    </th>
+                    <th>RECIPIENTS</th>
+                </tr>
+                <tr v-show="filterMode">
+                    <th>
+                        <v-text-field variant="solo" placeholder="Search"></v-text-field>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in EmailRecipientsData" :key="index">
+                    <td v-show="floatButtonData.editButtonActive" class="text-left" style="width: 20px">
+                        <v-checkbox
+                            style="display: flex;"
+                            color="indigo"
+                            :model-value="allSelected"
+                            :input-value="allSelected"
+                            @change="setSelected(index)"
+                        ></v-checkbox>
+                    </td>
+                    <td>{{ item.username }}</td>
+                    <td class="text">𝔱𝔢𝔰𝔱𝔦𝔫𝔤 𝔡𝔞𝔱𝔞</td>
+                </tr>
+            </tbody>
+        </v-table>
     </v-container>
 </template>
 
