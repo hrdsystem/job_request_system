ee3991e9dbef251c8a7674a9d0fb97519c61ae9b
Schwarzer <johnpaul.vitancol@gmail.com>
2025-08-06 16:04:40 +0800

    [Job Request] - email repicient model

--- app/Models/EmailRecipient.php
+++ app/Models/EmailRecipient.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EmailRecipient extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $appends = ['checked', 'disabled'];
+
+    public function getCheckedAttribute()
+    {
+        return false;
+    }
+
+    public function getDisabledAttribute()
+    {
+        return false;
+    }
 }
