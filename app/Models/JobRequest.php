99e5fc19166e598d2e57d2afd763c1a464918e9a
Schwarzer <johnpaul.vitancol@gmail.com>
2025-08-13 16:14:03 +0800

    [Job Request] - adding of new relationship for uploaded files

--- app/Models/JobRequest.php
+++ app/Models/JobRequest.php
@@ -33,4 +33,15 @@ class JobRequest extends Model
     public function requiredDocument(){
         return $this->hasMany('App\JobRequestRequirement', 'job_request_id');
     }
+
+    public function uploaded_file(){
+        return $this->hasMany('App\JobRequestUpload', 'request_id')
+        ->select(
+            'job_request_uploads.*',
+            'job_request_uploaded_files.upload_id',
+            'job_request_uploaded_files.orig_filename',
+            'job_request_uploaded_files.file_hash',
+        )
+        ->leftJoin('job_request_uploaded_files', 'job_request_uploaded_files.upload_id', 'job_request_uploads.id');
+    }
 }
