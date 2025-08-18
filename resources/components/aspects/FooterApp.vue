b53d7473be5c93d78981c17eda505ed42b12eff3
Schwarzer <johnpaul.vitancol@gmail.com>
2025-08-18 16:22:32 +0800

    [Job Request] - FooterApp component for showing of count on pages

---
+++ resources/components/aspects/FooterApp.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-footer dense inset color="white" app>
+        <span class="font-weight-bold" style="font-size: 12px;">Showing {{currentData}} of {{currentRecords}}</span>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        name: 'footerapp',
+        props:{
+            footerapp:{
+                type: Boolean,
+            },
+            currentData: 0,
+            currentRecords: 0,
+        }
+    }
+</script>
+
+<style scoped>
+    .v-footer{
+        z-index: 0 !important;
+        padding: 0 !important;
+    }
+</style>
