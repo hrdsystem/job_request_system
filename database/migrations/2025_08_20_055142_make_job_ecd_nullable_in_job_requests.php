4c323d2a9084de2ff98c5cd17897e1b8e5795b63
Schwarzer <johnpaul.vitancol@gmail.com>
2025-08-20 15:37:07 +0800

    [Job Request] - changing of job_ecd to nullable

---
+++ database/migrations/2025_08_20_055142_make_job_ecd_nullable_in_job_requests.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('job_requests', function (Blueprint $table) {
+            $table->date('job_ecd')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('job_requests', function (Blueprint $table) {
+            $table->date('job_ecd')->nullable(false)->change();
+
+        });
+    }
+};
