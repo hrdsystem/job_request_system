7ab770cd3ca16b94e24e22b1242afbe46b9ddc62
Schwarzer <johnpaul.vitancol@gmail.com>
2025-09-12 16:18:20 +0800

    [Job Request] - computed functions

--- resources/components/pages/JobRequestPage.vue
+++ resources/components/pages/JobRequestPage.vue
@@ -1510,6 +1510,24 @@ export default {
             return `${year}-${month}-${day}`; // Format: YYYY-MM-DD
         },
 
+        formatEcdDate(){
+            if(!this.tempEcd) return '';
+            const date = new Date(this.tempEcd);
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        },
+
+        formatChangeEcdDate(){
+            if(!this.currentECD) return '';
+            const date = new Date(this.currentECD);
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        },
+
         hasNewUploads(){
             return this.requiredDocuments.some(obj => obj.newUploads.length > 0)
         },
