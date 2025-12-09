4d7c3ffbab35f97d52433ee4594f9bcc88ab84da
Schwarzer <johnpaul.vitancol@gmail.com>
2025-12-09 16:12:07 +0800

    [Job Request\ - modified temporary server hosting

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
         host: '0.0.0.0',
         cors : {
             origin: 'http://10.169.141.202:8000',
+            // origin: '*',
             credentials: true
         },
         hmr: {
