10b813709408a712e3e9e82ede13698d5a455691
Schwarzer <johnpaul.vitancol@gmail.com>
2025-12-12 15:43:57 +0800

    [Job Request] - changing line of code for deployment of system

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,6 +11,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.$api = import.meta.env.VITE_APP_URL
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
